e93e3dc0aeacb4fa9d0db5813d7d59a08328a531
rezwana <rezwana3444@gmail.com>
2026-05-16 11:41:14 +0530

    fixed api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  team-task-manager-production-44de.up.railway.app/api
+  baseURL: "https://team-task-manager-production-44de.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
