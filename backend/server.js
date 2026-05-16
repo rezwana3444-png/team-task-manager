31d15c1ab9db713d162aee2bc06dda63fd939719
rezwana <rezwana3444@gmail.com>
2026-05-16 13:13:17 +0530

    fixed deployment

--- backend/server.js
+++ backend/server.js
@@ -48,16 +48,13 @@ app.use("/api/tasks", taskRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
   })
   .catch((err) => {
     console.log("MongoDB connection error:", err);
   });
-
-// ======================
-// START SERVER
-// ======================
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
