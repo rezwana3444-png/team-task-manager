59b11fb75226ca1f61d7a7b57295489a68319864
rezwana <rezwana3444@gmail.com>
2026-05-17 11:13:42 +0530

    fix server deploy

--- backend/server.js
+++ backend/server.js
@@ -49,9 +49,9 @@ mongoose.connect(process.env.MONGO_URI)
 
     const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
-});
+    app.listen(PORT, () => {
+      console.log("Server running on port", PORT);
+    });
   })
   .catch((err) => {
     console.log("MongoDB connection error:", err);
