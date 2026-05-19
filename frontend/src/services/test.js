820bc22c9892c3bcdae8bae64a3969425199d0c1
rezwana <rezwana3444@gmail.com>
2026-05-19 22:08:40 +0530

    fix railway deployment

---
+++ frontend/src/services/test.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+axios.post("https://team-task-manager-production-707c.up.railway.app/api/auth/login", {
+  email: "test@gmail.com",
+  password: "123456"
+})
+.then(res => console.log(res.data))
+.catch(err => console.log(err.response?.data || err.message));
