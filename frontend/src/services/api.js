d9a49429c06000d57886c23aa052cff34157f463
rezwana <rezwana3444@gmail.com>
2026-05-19 16:33:02 +0530

    Update to new Railway domain

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // Use the full URL that ends in .railway.app
-  baseURL: "https://team-task-manager-production-44de.up.railway.app/api"
+  baseURL: "https://team-task-manager-production-076e.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
