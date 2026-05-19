9bdd8288a114a988b679db8d376abc6c5b791283
rezwana <rezwana3444@gmail.com>
2026-05-19 15:56:43 +0530

    Set correct API baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,15 @@
 import axios from "axios";
 
-console.log("API FILE LOADED");
-
 const API = axios.create({
+  // Use the full URL that ends in .railway.app
   baseURL: "https://team-task-manager-production-44de.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
-  console.log("TOKEN SENT:", token);
-
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
   return req;
 });
 
