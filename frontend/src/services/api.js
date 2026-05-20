0f356eec315b51b9db5488b64fc3e9c46a7e3311
rezwana <rezwana3444@gmail.com>
2026-05-20 15:49:02 +0530

    fix frontend env and auth

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,23 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Attach token automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  console.log("🔥 INTERCEPTOR RUNNING");
-  console.log("TOKEN:", token);
-
   if (token) {
-    config.headers = config.headers || {};
-
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
-
-  console.log("HEADERS:", config.headers);
 
   return config;
 });
