eda9eced153cbb9a9aac437ee7edbadf2dbbeed5
rezwana <rezwana3444@gmail.com>
2026-05-20 15:03:51 +0530

    fix auth header

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,22 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
 
+// Attach token automatically
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  console.log("🔥 INTERCEPTOR RUNNING");
+  console.log("TOKEN:", token);
+
+  if (token) {
+    config.headers = config.headers || {};
+
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  console.log("HEADERS:", config.headers);
+
+  return config;
+});
+
 export default API;
