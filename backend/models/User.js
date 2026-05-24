564edfb209e6c303b5d4166b36fa10b760d33c1a
rezwana <rezwana3444@gmail.com>
2026-05-24 12:44:22 +0530

    Fixed role enum

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,28 @@
 // models/User.js
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['Admin', 'Member'], default: 'Member' }
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "member"],
+    default: "member",
+  },
 });
+
 module.exports = mongoose.model("User", userSchema);
